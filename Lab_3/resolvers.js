bb327199031a7d3fc05e6a3dc451d530475c7890
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-10-16 04:19:17 -0400

    done

--- Lab_3/resolvers.js
+++ Lab_3/resolvers.js
@@ -1,5 +1,7 @@
 import { GraphQLError } from "graphql";
-
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat.js";
+dayjs.extend(customParseFormat);
 import {
   authors as authorsCollection,
   books as booksCollection,
@@ -13,6 +15,141 @@ client.on("error", (err) => {
 client.connect().then(() => {});
 
 import { v4 as uuid, validate as isUUIDValid } from "uuid";
+function isValidAuthorName(name) {
+  if (name.trim() !== "") return /^[A-Za-z]+$/.test(name);
+  return false;
+}
+function isValidDate(dateString) {
+  const dateFormatsToCheck = [
+    "MM/DD/YYYY",
+    "MM-DD-YYYY",
+    "D-M-YYYY",
+    "DD-MM-YYYY",
+  ];
+  for (const format of dateFormatsToCheck) {
+    const date = dayjs(dateString, { format, strict: true });
+    if (date.isValid()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isValidStateAbbreviation(state) {
+  const validStateAbbreviations = [
+    "AL",
+    "AK",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "FL",
+    "GA",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "OH",
+    "OK",
+    "OR",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+  ];
+  return validStateAbbreviations.includes(state);
+}
+
+function isAuthorValidNew(author) {
+  let isValid = false;
+  if (
+    isUUIDValid(author._id) &&
+    isValidAuthorName(author.first_name.trim()) &&
+    isValidAuthorName(author.last_name.trim()) &&
+    isValidDate(author.date_of_birth.trim()) &&
+    isValidStateAbbreviation(author.hometownState.trim()) &&
+    author.hometownCity.trim() !== ""
+  ) {
+    author._id = author._id.trim();
+    author.first_name = author.first_name.trim();
+    author.last_name = author.last_name.trim();
+    author.hometownCity = author.hometownCity.trim();
+    author.hometownState = author.hometownState.trim();
+    isValid = true;
+  }
+  return { author, isValid };
+}
+function isAuthorValidEdit(inputAuthor) {
+  let isValid = true; // Assume it's valid by default
+  const author = {};
+  author._id = inputAuthor._id.trim();
+  isValid = isValid && isUUIDValid(inputAuthor._id);
+
+  if (inputAuthor.first_name && isValid) {
+    author.first_name = inputAuthor.first_name.trim();
+    isValid = isValid && isValidAuthorName(inputAuthor.first_name);
+  }
+
+  if (inputAuthor.last_name && isValid) {
+    author.last_name = inputAuthor.last_name.trim();
+    isValid = isValid && isValidAuthorName(inputAuthor.last_name);
+  }
+
+  if (inputAuthor.date_of_birth && isValid) {
+    author.date_of_birth = inputAuthor.date_of_birth.trim();
+    isValid = isValid && isValidDate(inputAuthor.date_of_birth);
+  }
+
+  if (inputAuthor.hometownState && isValid) {
+    author.hometownState = inputAuthor.hometownState.trim();
+    isValid = isValid && isValidStateAbbreviation(inputAuthor.hometownState);
+  }
+
+  if (inputAuthor.hometownCity && isValid) {
+    author.hometownCity = inputAuthor.hometownCity.trim();
+    isValid = isValid && inputAuthor.hometownCity !== "";
+  }
+
+  return { author, isValid };
+}
+
+//https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s13.html
+function isValidISBN(isbn) {
+  const isbnRegex =
+    /^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/;
+  return isbnRegex.test(isbn);
+}
 
 export const resolvers = {
   Query: {
@@ -265,6 +402,327 @@ export const resolvers = {
       }
     },
   },
+  Mutation: {
+    addAuthor: async (_, args) => {
+      let newAuthor = {
+        _id: uuid(),
+        first_name: args.first_name,
+        last_name: args.last_name,
+        date_of_birth: args.date_of_birth,
+        hometownCity: args.hometownCity,
+        hometownState: args.hometownState,
+        numOfBooks: 0,
+        books: [],
+      };
+      const result = isAuthorValidNew(newAuthor);
+      if (!result.isValid) {
+        throw new GraphQLError(`incorrect inputs`, {
+          extensions: { code: "BAD_INPUT" },
+        });
+      }
+      newAuthor = result.author;
+      const authors = await authorsCollection();
+      let insertedAuthor = await authors.insertOne(newAuthor);
+      if (!insertedAuthor.acknowledged || !insertedAuthor.insertedId) {
+        throw new GraphQLError(`Could not Add Author`, {
+          extensions: { code: "INTERNAL_SERVER_ERROR" },
+        });
+      }
+      await client.set(newAuthor._id, JSON.stringify(newAuthor));
+      await client.del("authors");
+      await resolvers.Query.authors();
+      return newAuthor;
+    },
+    editAuthor: async (_, args) => {
+      if (!args.length === 0) {
+        throw new GraphQLError(`edit author needs inputs`, {
+          extensions: { code: "BAD_INPUT" },
+        });
+      }
+
+      let newAuthor = {
+        _id: args._id,
+        first_name: args?.first_name ?? "",
+        last_name: args?.last_name ?? "",
+        date_of_birth: args?.date_of_birth ?? "",
+        hometownCity: args?.hometownCity ?? "",
+        hometownState: args?.hometownState ?? "",
+      };
+      const result = isAuthorValidEdit(newAuthor);
+      if (!result.isValid) {
+        throw new GraphQLError(`incorrect inputs`, {
+          extensions: { code: "BAD_INPUT" },
+        });
+      }
+      newAuthor = result.author;
+      const authors = await authorsCollection();
+      const author = await authors
+        .find({ _id: newAuthor._id.trim() })
+        .toArray();
+      if (!author || author.length === 0) {
+        throw new GraphQLError("Author Not Found", {
+          extensions: { code: "NOT_FOUND" },
+        });
+      }
+      author[0] = { ...author[0], ...newAuthor };
+      const updatedAuthor = { ...author[0] };
+      delete updatedAuthor._id;
+      let updateResult = await authors.updateOne(
+        { _id: author[0]._id },
+        { $set: updatedAuthor }
+      );
+      if (updateResult.acknowledged && updateResult.modifiedCount === 1) {
+        await client.set(author[0]._id, JSON.stringify(author[0]));
+        await client.del("authors");
+        await resolvers.Query.authors();
+        return author[0];
+      } else {
+        throw new GraphQLError("Author update failed", {
+          extensions: { code: "UPDATE_FAILED" },
+        });
+      }
+    },
+    removeAuthor: async (_, args) => {
+      if (!isUUIDValid(args?._id?.trim())) {
+        throw new GraphQLError(`bad author id`, {
+          extensions: { code: "BAD_INPUT" },
+        });
+      }
+
+      const authors = await authorsCollection();
+      const deletedauthor = await authors.findOneAndDelete({
+        _id: args._id.trim(),
+      });
+
+      if (!deletedauthor) {
+        throw new GraphQLError(
+          `Could not delete author with _id of ${args._id}`,
+          {
+            extensions: { code: "NOT_FOUND" },
+          }
+        );
+      }
+      await client.del(args._id.trim());
+      await client.del("authors");
+      await resolvers.Query.authors();
+
+      if (deletedauthor.books.length > 0) {
+        const books = await booksCollection();
+        let deletedBooks = await books.deleteMany({
+          _id: { $in: deletedauthor.books },
+        });
+        if (deletedBooks.deletedCount > 0) {
+          deletedauthor.books.forEach(async (bookId) => {
+            await client.del(bookId);
+          });
+        }
+
+        await client.del("books");
+        await resolvers.Query.books();
+      }
+      return deletedauthor;
+    },
+    addBook: async (_, args) => {
+      if (
+        !args.title.trim() ||
+        !args.genres.every((genre) => genre.trim()) ||
+        !args.publicationDate.trim() ||
+        !args.publisher.trim() ||
+        !args.summary.trim() ||
+        !args.isbn.trim() ||
+        !args.language.trim() ||
+        args.pageCount <= 0 ||
+        args.price <= 0
+      ) {
+        throw new GraphQLError("Invalid input parameters", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+      if (!isValidDate(args.publicationDate)) {
+        throw new GraphQLError("Invalid publication date", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+      if (isNaN(args.price)) {
+        throw new GraphQLError("Price must be a number", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+      if (!isValidISBN(args.isbn)) {
+        throw new GraphQLError("Invalid ISBN format", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+      if (!Number.isInteger(args.pageCount) || args.pageCount <= 0) {
+        throw new GraphQLError("Invalid pageCount", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+      const authors = await authorsCollection();
+      const author = await authors.findOne({ _id: args.authorId });
+      if (!author) {
+        throw new GraphQLError("Author not found", {
+          extensions: { code: "NOT_FOUND" },
+        });
+      }
+
+      // Insert the new book into MongoDB
+      const newBook = {
+        _id: uuid(),
+        title: args.title,
+        genres: args.genres,
+        publicationDate: args.publicationDate,
+        publisher: args.publisher,
+        summary: args.summary,
+        isbn: args.isbn,
+        language: args.language,
+        pageCount: args.pageCount,
+        price: args.price,
+        format: args.format,
+        authorId: args.authorId,
+      };
+      const books = await booksCollection();
+      const result = await books.insertOne(newBook);
+
+      if (result.acknowledged && result.insertedId) {
+        authors.updateOne(
+          { _id: args.authorId },
+          { $push: { books: result.insertedId } }
+        );
+        await client.del(args.authorId.trim());
+        await resolvers.Query.getAuthorById(_, author);
+        await client.del("authors");
+        await resolvers.Query.authors();
+        await client.set(`${result.insertedId}`, JSON.stringify(newBook));
+        await client.del("books");
+        await resolvers.Query.books();
+        return newBook;
+      } else {
+        throw new GraphQLError("Book insertion failed", {
+          extensions: { code: "INTERNAL_SERVER_ERROR" },
+        });
+      }
+    },
+    editBook: async (_, args) => {
+      if (!isUUIDValid(args?._id?.trim())) {
+        throw new GraphQLError(`bad author id`, {
+          extensions: { code: "BAD_INPUT" },
+        });
+      }
+
+      const books = await booksCollection();
+      const authors = await authorsCollection();
+      const existingBook = await books.findOne({ _id: args._id });
+      if (!existingBook) {
+        throw new GraphQLError("Book not found", {
+          extensions: { code: "NOT_FOUND" },
+        });
+      }
+
+      if (
+        (args.title && !args.title.trim()) ||
+        (args.genres && args.genres.some((genre) => !genre.trim())) ||
+        (args.publicationDate && !isValidDate(args.publicationDate)) ||
+        (args.price && isNaN(args.price)) ||
+        (args.isbn && !isValidISBN(args.isbn)) ||
+        (args.pageCount &&
+          (!Number.isInteger(args.pageCount) || args.pageCount <= 0))
+      ) {
+        throw new GraphQLError("Invalid input parameters", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+
+      if (args.authorId && args.authorId !== existingBook.authorId) {
+        await authors.updateOne(
+          { _id: existingBook.authorId },
+          { $pull: { books: existingBook._id } }
+        );
+
+        await authors.updateOne(
+          { _id: args.authorId },
+          { $push: { books: existingBook._id } }
+        );
+      }
+
+      const updatedBook = {
+        ...existingBook,
+        title: args.title || existingBook.title,
+        genres: args.genres || existingBook.genres,
+        publicationDate: args.publicationDate || existingBook.publicationDate,
+        publisher: args.publisher || existingBook.publisher,
+        summary: args.summary || existingBook.summary,
+        isbn: args.isbn || existingBook.isbn,
+        language: args.language || existingBook.language,
+        pageCount: args.pageCount || existingBook.pageCount,
+        price: args.price || existingBook.price,
+        format: args.format || existingBook.format,
+        authorId: args.authorId || existingBook.authorId,
+      };
+
+      const result = await books.updateOne(
+        { _id: args._id },
+        { $set: updatedBook }
+      );
+
+      if (result.modifiedCount === 1) {
+        await client.del(`${args._id}`);
+        await client.set(`${args._id}`, JSON.stringify(updatedBook));
+        if (existingBook.authorId !== updatedBook.authorId) {
+          await client.del(`${existingBook.authorId}`);
+          await resolvers.Query.getAuthorById(_, {
+            _id: existingBook.authorId,
+          });
+        }
+        await client.del(`${updatedBook.authorId}`);
+        await resolvers.Query.getAuthorById(_, { _id: updatedBook.authorId });
+
+        await client.del("authors");
+        await resolvers.Query.authors();
+        await client.del("books");
+        await resolvers.Query.books();
+
+        return updatedBook;
+      } else {
+        throw new GraphQLError("Book update failed", {
+          extensions: { code: "INTERNAL_SERVER_ERROR" },
+        });
+      }
+    },
+    removeBook: async (_, args) => {
+      if (!isUUIDValid(args?._id?.trim())) {
+        throw new GraphQLError(`bad author id`, {
+          extensions: { code: "BAD_INPUT" },
+        });
+      }
+      const books = await booksCollection();
+      const existingBook = await books.findOne({ _id: args._id });
+      if (!existingBook) {
+        throw new GraphQLError("Book not found", {
+          extensions: { code: "NOT_FOUND" },
+        });
+      }
+      const deleteResult = await books.deleteOne({ _id: args._id });
+      if (deleteResult.deletedCount === 1) {
+        const authors = await authorsCollection();
+        await client.del(`${args._id}`);
+        await authors.updateOne(
+          { _id: existingBook.authorId },
+          { $pull: { books: existingBook._id } }
+        );
+        await resolvers.Query.getAuthorById(_, { _id: existingBook.authorId });
+        await client.del("authors");
+        await resolvers.Query.authors();
+        await client.del("books");
+        await resolvers.Query.books();
+        return existingBook;
+      } else {
+        throw new GraphQLError("Book deletion failed", {
+          extensions: { code: "INTERNAL_SERVER_ERROR" },
+        });
+      }
+    },
+  },
   Book: {
     author: async (_, args) => {
       const authors = await authorsCollection();
@@ -279,6 +737,13 @@ export const resolvers = {
       const books = await booksCollection();
       const written = await books.find({ authorId: _._id }).toArray();
       return written.slice(0, args.limit);
+    },
+    numOfBooks: async (_, args) => {
+      const books = await booksCollection();
+      const numOfBooks = await books.count({
+        authorId: _._id,
+      });
+      return numOfBooks;
     },
   },
 };
--- Lab_3/typeDefs.js
+++ Lab_3/typeDefs.js
@@ -36,4 +36,55 @@ type Query {
   booksByPriceRange (min: Float!, max: Float!) : [Book],
   searchAuthorsByName (searchTerm: String!): [Author]
 }
+
+type Mutation {
+  addAuthor(
+    first_name: String!,
+    last_name: String!,
+    date_of_birth: String!,
+    hometownCity: String!,
+    hometownState: String!
+  ) : Author,
+  editAuthor(
+    _id: String!,
+    first_name: String,
+    last_name: String,
+    date_of_birth: String,
+    hometownCity: String,
+    hometownState: String
+  ) : Author,
+  removeAuthor(
+    _id: String!
+  ) : Author,
+  addBook(
+    title: String!,
+    genres: [String!]!,
+    publicationDate: String!,
+    publisher: String!,
+    summary: String!,
+    isbn: String!,
+    language: String!,
+    pageCount: Int!,
+    price: Float!,
+    format: [String!]!,
+    authorId: String!
+  ) : Book,
+  editBook(
+    _id: String!,
+    title: String,
+    genres: [String],
+    publicationDate: String,
+    publisher: String,
+    summary: String,
+    isbn: String,
+    language: String,
+    pageCount: Int,
+    price: Float,
+    format: [String],
+    authorId: String
+  ) : Book,
+  removeBook(
+    _id: String!
+  ) : Book
+}
   `;
