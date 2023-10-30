b93603e0b76dede603a433261ef9e40a48181940
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-10-29 22:33:33 -0400

    code base

--- Lab_4/Sanjeet_Vinod_Jain_CS554_A_Lab_4/src/App.jsx
+++ Lab_4/Sanjeet_Vinod_Jain_CS554_A_Lab_4/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./components/Home";
+import { Route, Link, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <header className="App-header">
+          <img src={viteLogo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to the TV Maze API</h1>
+          <Link className="showlink" to="/">
+            Home
+          </Link>
+        </header>
+        <br />
+        <br />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
