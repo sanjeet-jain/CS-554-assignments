c86a6b7b2e729dbd008cf94d15b45a948e48b018
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-08 19:00:49 -0500

    cleanup

--- Lab_7/client/src/lib/query.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const GET_COMICS = gql`
-  query Query($test: Int) {
-    test(test: $test)
-  }
-`;
-
-// At minimum, every comic's individual page should show its title, an Image, the description, their onSaleDate, and their printPrice.
--- Lab_7/config/settings.js
+++
@@ -1,4 +0,0 @@
-export const settings = {
-  MARVEL_API_PUBLIC_KEY: "6b945cea7609cabbd4d4b782ff6e72ef",
-  MARVEL_API_PRIVATE_KEY: "655619f733d93fd4ac21a1c7649e191fade6fe20",
-};
